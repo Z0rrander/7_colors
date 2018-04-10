df04dcff214200f9326aa671cba1ffb1451604b3
xelofox <38127459+xelofox@users.noreply.github.com>
2018-04-10 08:31:15 +0200

    Update random_possible_color.c

--- Code/random_possible_color.c
+++ Code/random_possible_color.c
@@ -19,47 +19,117 @@ char random_possible_color(char perso, char *tableau, int taille){ //sort une co
 			}
 		}
 	}
-	char couleurs[7] //tableau dans lequel on stocke les couleurs
-	int compte2=0
-	char actualcolor
+	char couleurs[7]; //tableau dans lequel on stocke les couleurs
+	int compte2=0;
+	char actualcolor;
+	int ilyest=0;// variable indiquant si une couleur est dans un tableau ou non
 	
+	while (compte2<6){ //condition d'arret évitant de chercher plus de couleur si on les as toutes
+		
 	for(k=0;k<compte;k++){
 		int x=positions[k][1];
 		int y=positions[k][2];
 		
-		if((x-1!=-1) && (x-1!=taille)){
-			if((y!=-1) && (y!=taille)){
-					for(l=0; l<compte2; l++){
-						actualcolor=get_cell(x-1,y, *tableau)
-						if((actualcolor==couleur[l]) && (actualcolor!=perso){
-						set_cell(x-1,y,perso, *tableau);
-					}
+		if((x-1!=-1)){
+			for(l=0; l<compte2; l++){
+				actualcolor=get_cell(x-1,y, *tableau);
+				if((actualcolor==couleurs[l]) && (actualcolor!='^') && (actualcolor!='v')){
+					ilyest=1;
 				}
 			}
-		}
-		if((x+1!=-1) && (x+1!=taille)){
-			if((y!=-1) && (y!=taille)){
-				if(get_cell(x+1,y, *tableau)==couleur){
-					set_cell(x+1,y,perso, tableau);
-				}
+			if(ilyest==0){
+				couleurs[compte2]=actualcolor;
+				compte2++;
 			}
+			ilyest=0;
 		}
 		
-		if((y+1!=-1) && (y+1!=taille)){
-			if((x!=-1) && (x!=taille)){
-				if(get_cell(x,y+1, *tableau)==couleur){
-					set_cell(x,y+1,perso, tableau);
+		
+		
+		if((x+1!=taille)){
+			for(l=0; l<compte2; l++){
+				actualcolor=get_cell(x+1,y, *tableau);
+				if((actualcolor==couleurs[l]) && (actualcolor!='^') && (actualcolor!='v')){
+					ilyest=1;
 				}
 			}
+			if(ilyest==0){
+				couleurs[compte2]=actualcolor;
+				compte2++;
+			}
+			ilyest=0;
 		}
-		if((y-1!=-1) && (y+1!=taille)){
-			if((x!=-1) && (x!=taille)){
-				if(get_cell(x,y-1, *tableau)==couleur){
-					set_cell(x,y-1,perso, *tableau);
+		
+		
+		
+		if((y-1!=-1)){
+			for(l=0; l<compte2; l++){
+				actualcolor=get_cell(x,y-1, *tableau);
+				if((actualcolor==couleurs[l]) && (actualcolor!='^') && (actualcolor!='v')){
+					ilyest=1;
 				}
 			}
+			if(ilyest==0){
+				couleurs[compte2]=actualcolor;
+				compte2++;
+			}
+			ilyest=0;
+		}
+		
+		
+		
+		if((y+1!=taille)){
+			for(l=0; l<compte2; l++){
+				actualcolor=get_cell(x,y+1, *tableau);
+				if((actualcolor==couleurs[l]) && (actualcolor!='^') && (actualcolor!='v')){
+					ilyest=1;
+				}
+			}
+			if(ilyest==0){
+				couleurs[compte2]=actualcolor;
+				compte2++;
+			}
+			ilyest=0;
 		}
 	}
+	}
+	
+	srand(time(NULL)); 
+	int nb_alea; //Nombre aleatoire
+	int inf = 1, sup = compte2; //Bornes inférieures et supérieures pour avoir une couleur aléatoire.
+	nb_alea = rand()%((sup + 1) - inf ) + inf;
+	char color;
+	switch(nb_alea){ //On regarde chaque cas pour déterminer la couleur 
+							 //choisie aleatoirement				
+					case 1 : 
+						color=couleurs[1];
+						break;
+						
+					case 2 : 
+						color=couleurs[2];
+						break;
+						
+					case 3 : 
+						color=couleurs[3];
+						break;
+						
+					case 4 : 
+						color=couleurs[4];
+						break;
+						
+					case 5 : 
+						color=couleurs[5];
+						break;
+						
+					case 6 : 
+						color=couleurs[6];
+						break;
+						
+					case 7 : 
+						color=couleurs[7];
+						break;	
+					}
+	return color;
+	
 }
 
-}
