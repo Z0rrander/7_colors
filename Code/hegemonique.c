63e3d1589a9b864ae31731f40636b19d6f0b95fe
xelofox <38127459+xelofox@users.noreply.github.com>
2018-04-11 13:06:34 +0200

    Add files via upload
    jte beze

--- Code/hegemonique.c
+++ Code/hegemonique.c
@@ -1,45 +1,75 @@
-#include "prototype.h"
-
 char hegemonique(char perso, char *tableau, int taille){ //va choisir la couleur qui est en majorité à sa disposition sur son bord
 	int positions[taille*taille][2]; // tableau dans lequel on va stocker les positions actuelles sur les bords possédées par le joueur
 	int compte=0; // un compte utilisé pour remplir le tableau avec les positions
-	int i, j, k, l; 
-			
+	int i, j, k, l,m; 
+	int pos_horizon, pos_vertical;  // position horizontal et vertical maximal du joeur		
 	//on va chercher les positions du joueur
 	char actualcolor='z';
 	if (perso=='v'){ // on prend ici la position la )plus à droite sur chaque ligne
 		j=0;
-		for(i=0;i<taille;i++){
-			for(j=0;j<taille;j++){
+		i=0;
+		while(i!=-1){
+			while (j!=taille){
 				actualcolor=get_cell(i,j, tableau);
 				if(actualcolor== perso){
 					positions[compte][1]=i;
 					positions[compte][2]=j;
 				}
+				if((j==taille-1) && (compte<j)){ // si on n'a trouvé aucune position sur cette ligne
+					pos_horizon=positions[compte][1];
+					pos_vertical=positions[compte][2]=j;
+					j=taille-1;
+					i=0;
+				}
+				if (positions[compte][1]==i){ // on vérifie qu'on a ajouté une position
+					compte=compte+1;
+				}
+				j++;
 			}
-			if (positions[compte][1]==i){ // on vérifie qu'on a ajouté une position
-				compte=compte+1;
+			j=0;
+			i=i-1;
+		}
+		for(m=0;m<pos_vertical;m++){
+			if(get_cell(pos_horizon,m, tableau)== perso){
+				positions[compte][1]=i;
+				positions[compte][2]=j;
+				compte++;
 			}
 		}
 	}
 	
 	if (perso=='^'){ // on prend ici la position la plus à gauche sur chaque ligne
 		j=taille-1;
-		for(i=0;i<taille;i++){
+		while(i!=taille){
 			while(j!=-1){
 				actualcolor=get_cell(i,j, tableau);
 				if(actualcolor== perso){
 					positions[compte][1]=i;
 					positions[compte][2]=j;
 				}
+				if((j==0) && (compte<j)){ // si on n'a trouvé aucune position sur cette ligne
+					pos_horizon=positions[compte][1];
+					pos_vertical=positions[compte][2]=j;
+					j=0;
+					i=taille-1;
+				}
 				j=j-1;
 			}
 			j=taille-1;
+			i++;
 			if (positions[compte][1]==i){ // on vérifie qu'on a ajouté une position
 				compte=compte+1;
 			}
 		}
+		for(m=pos_vertical-1;m>-1;m--){
+			if(get_cell(pos_horizon,m, tableau)== perso){
+				positions[compte][1]=i;
+				positions[compte][2]=j;
+				compte++;
+			}
+		}
 	}
+
 	
 	char couleurs[7];
 	couleurs[0]='A'; couleurs[1]='B'; couleurs[2]='C'; couleurs[3]='D'; couleurs[4]='E'; couleurs[5]='F'; couleurs[6]='G';
@@ -100,6 +130,6 @@ char hegemonique(char perso, char *tableau, int taille){ //va choisir la couleur
 			nb_max=couleur_compte[l];
 		}
 	}
-	
+
 	return couleurs[max];
-}
+	}
